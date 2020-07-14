c5eaf27d32c63d71e73078824ea3e30355366d79
miran248 <miran248+github@cflow.io>
2020-07-14 11:50:15 +0200

    chore: play around with some experiments

--- hello-watch/Prancercise Tracker Starter/Prancercise Tracker/ProfileViewController.swift
+++ hello-watch/Prancercise Tracker Starter/Prancercise Tracker/ProfileViewController.swift
@@ -32,18 +32,18 @@ import UIKit
 import HealthKit
 
 class ProfileViewController: UITableViewController {
-    
+
   private enum ProfileSection: Int {
     case ageSexBloodType
     case weightHeightBMI
     case readHealthKitData
     case saveBMI
   }
-  
+
   private enum ProfileDataError: Error {
-    
+
     case missingBodyMassIndex
-    
+
     var localizedDescription: String {
       switch self {
       case .missingBodyMassIndex:
@@ -51,22 +51,22 @@ class ProfileViewController: UITableViewController {
       }
     }
   }
-  
+
   @IBOutlet private var ageLabel:UILabel!
   @IBOutlet private var bloodTypeLabel:UILabel!
   @IBOutlet private var biologicalSexLabel:UILabel!
   @IBOutlet private var weightLabel:UILabel!
   @IBOutlet private var heightLabel:UILabel!
   @IBOutlet private var bodyMassIndexLabel:UILabel!
-  
+
   private let userHealthProfile = UserHealthProfile()
-  
+
   private func updateHealthInfo() {
     loadAndDisplayAgeSexAndBloodType()
     loadAndDisplayMostRecentWeight()
     loadAndDisplayMostRecentHeight()
   }
-  
+
   private func loadAndDisplayAgeSexAndBloodType() {
     do {
       let userAgeSexAndBloodType = try ProfileDataStore.getAgeSexAndBloodType()
@@ -78,7 +78,7 @@ class ProfileViewController: UITableViewController {
       self.displayAlert(for: error)
     }
   }
-  
+
   private func updateLabels() {
     if let age = userHealthProfile.age {
       ageLabel.text = "\(age)"
@@ -91,7 +91,7 @@ class ProfileViewController: UITableViewController {
     if let bloodType = userHealthProfile.bloodType {
       bloodTypeLabel.text = bloodType.stringRepresentation
     }
-    
+
     if let weight = userHealthProfile.weightInKilograms {
       let weightFormatter = MassFormatter()
       weightFormatter.isForPersonMassUse = true
@@ -102,13 +102,13 @@ class ProfileViewController: UITableViewController {
       heightFormatter.isForPersonHeightUse = true
       heightLabel.text = heightFormatter.string(fromMeters: height)
     }
-       
+
 //    if let bodyMassIndex = userHealthProfile.bodyMassIndex {
 //      bodyMassIndexLabel.text = String(format: "%.02f", bodyMassIndex)
 //    }
 
   }
-  
+
   private func loadAndDisplayMostRecentHeight() {
     print("loadAndDisplayMostRecentHeight")
     //1. Use HealthKit to create the Height Sample Type
@@ -116,26 +116,26 @@ class ProfileViewController: UITableViewController {
       print("Height Sample Type is no longer available in HealthKit")
       return
     }
-    
+
     guard let heartRateSampleType = HKSampleType.quantityType(forIdentifier: .heartRate) else {
       print("Height Sample Type is no longer available in HealthKit")
       return
     }
-    
-    
+
+
     ProfileDataStore.getMostRecentSample(for: heartRateSampleType) { (sample, error) in
-        
+
       guard let sample = sample else {
-          
+
         if let error = error {
           self.displayAlert(for: error)
         }
-            
+
         return
       }
-        
+
         print("heart reate \(sample.quantity) \(error)")
-          
+
       //2. Convert the height sample to meters, save to the profile model,
       //   and update the user interface.
 //      let heightInMeters = sample.quantity.doubleValue(for: HKUnit.meter())
@@ -143,76 +143,76 @@ class ProfileViewController: UITableViewController {
 //        print("height in meters \(heightInMeters)")
 //        self.updateLabels()
     }
-    
-        
-//    ProfileDataStore.getMostRecentSample(for: heightSampleType) { (sample, error) in
-//        print("getMostRecentSample \(sample) \(error)")
-//      guard let sample = sample else {
-//          
-//        if let error = error {
-//          self.displayAlert(for: error)
-//        }
-//            
-//        return
-//      }
-//          
-//      //2. Convert the height sample to meters, save to the profile model,
-//      //   and update the user interface.
-//      let heightInMeters = sample.quantity.doubleValue(for: HKUnit.meter())
-//      self.userHealthProfile.heightInMeters = heightInMeters
-//        print("height in meters \(heightInMeters)")
-//      self.updateLabels()
-//    }
+
+
+    ProfileDataStore.getMostRecentSample(for: heightSampleType) { (sample, error) in
+        print("getMostRecentSample \(sample) \(error)")
+      guard let sample = sample else {
+
+        if let error = error {
+          self.displayAlert(for: error)
+        }
+
+        return
+      }
+
+      //2. Convert the height sample to meters, save to the profile model,
+      //   and update the user interface.
+      let heightInMeters = sample.quantity.doubleValue(for: HKUnit.meter())
+      self.userHealthProfile.heightInMeters = heightInMeters
+        print("height in meters \(heightInMeters)")
+      self.updateLabels()
+    }
 
   }
-  
+
   private func loadAndDisplayMostRecentWeight() {
     guard let weightSampleType = HKSampleType.quantityType(forIdentifier: .bodyMass) else {
       print("Body Mass Sample Type is no longer available in HealthKit")
       return
     }
-        
+
     ProfileDataStore.getMostRecentSample(for: weightSampleType) { (sample, error) in
-          
+
       guard let sample = sample else {
-            
+
         if let error = error {
           self.displayAlert(for: error)
         }
         return
       }
-          
+
       let weightInKilograms = sample.quantity.doubleValue(for: HKUnit.gramUnit(with: .kilo))
       self.userHealthProfile.weightInKilograms = weightInKilograms
       self.updateLabels()
     }
 
   }
-  
+
   private func saveBodyMassIndexToHealthKit() {
 
 
   }
-  
+
   private func displayAlert(for error: Error) {
-    
+
     let alert = UIAlertController(title: nil,
                                   message: error.localizedDescription,
                                   preferredStyle: .alert)
-    
+
     alert.addAction(UIAlertAction(title: "O.K.",
                                   style: .default,
                                   handler: nil))
-    
+
     present(alert, animated: true, completion: nil)
   }
-  
+
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    
+
     guard let section = ProfileSection(rawValue: indexPath.section) else {
       fatalError("A ProfileSection should map to the index path's section")
     }
-    
+
     switch section {
     case .saveBMI:
       saveBodyMassIndexToHealthKit()
