a7dc2c69e1acd482e3c27b72cfdf38a2b50a6cc8
miran248 <miran248+github@cflow.io>
2022-07-09 15:46:51 +0200

    fix: fix csv iterations endpoint

--- heartrate/pages/api/iterations-csv.js
+++ heartrate/pages/api/iterations-csv.js
@@ -1,4 +1,4 @@
-import { stringify } from "csv/sync";
+import { stringify } from "csv-stringify/lib/sync";
 
 import { loadIterations } from "../../store-web";
 
@@ -7,28 +7,32 @@ export default async (req, res) => {
 
   const output = data
     .filter((item) => item.valid && item.trainable)
+    .sort((a, b) => a.timestamp - b.timestamp)
     .map((item, e, items) => {
       let ma = 0;
       for (let s = Math.max(e - 3, 0); s <= e; ++s) {
         ma += items[s].actualRank;
       }
       ma /= Math.min(e + 1, 4);
-      return {
-        date: item.title.split("T")[0],
-        "avg BPM animal": item.aggregate.animal.bpm.toFixed(2),
-        "avg BPM human": item.aggregate.animal.bpm.toFixed(2),
-        "delta BPM": item.delta.toFixed(2),
-        class: item.actualRank,
-        "moving 4": ma.toFixed(2),
-        start: item.aggregate.start,
-        stop: item.aggregate.stop,
-      };
-    });
+      item.ma = ma;
+      return item;
+    })
+    .sort((a, b) => b.timestamp - a.timestamp)
+    .map((item) => ({
+      date: item.title.split("T")[0],
+      "avg BPM animal": item.aggregate.animal.bpm.toFixed(2),
+      "avg BPM human": item.aggregate.animal.bpm.toFixed(2),
+      "delta BPM": item.delta.toFixed(2),
+      class: item.actualRank,
+      "moving 4": item.ma.toFixed(2),
+      start: item.aggregate.start,
+      stop: item.aggregate.stop,
+    }));
 
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.statusCode = 200;
-  res.text(
+  res.send(
     stringify(output, { delimiter: ",", encoding: "utf8", header: true })
   );
 };
